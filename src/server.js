9efe96955ecc4efb0a5c589ba884b9db95a0fa65
ken <ken2020n@users.noreply.github.com>
2022-08-13 03:09:44 +0900

    initialize express server with db connection

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const knex = require("./knex");
+
+const setupServer = () => {
+    const app = express();
+    app.use(express.json());
+
+    app.get("/test", async (req, res) => {
+        res.status(200).json({
+            message: "API server connection successful.",
+        });
+    });
+
+    app.get("/dbtest", async (req, res) => {
+        let result = await knex.raw(`SELECT 1;`);
+        if (result.rowCount === 1) {
+            res.status(200).json({
+                message: "Database connection successful.",
+            });
+        } else {
+            res.status(500).json({
+                message: "Failed to connect to database .",
+            });
+        }
+    });
+
+    return app;
+};
+
+module.exports = { setupServer };
