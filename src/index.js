998535a59ed3571da97b40f43ae062714c4ff4f8
ken <ken2020n@users.noreply.github.com>
2022-08-13 13:51:16 +0900

    fix: db connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const { setupServer } = require("./server");
+const {setupServer} = require("./server");
+const db = require("./knex");
 
 const server = setupServer();
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Server listening on Port: ", PORT);
-});
+
+
+(async () => {
+    try {
+        await db.migrate.latest();
+        server.listen(PORT, () => {
+            console.log("Server listening on Port: ", PORT);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+
+
